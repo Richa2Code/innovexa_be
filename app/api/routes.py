755ff493f288a9469b998dacf78cc63ca0dda040
Richa Makwana <richamakwana13@gmail.com>
2026-09-05 16:44:14 +0530

    Updt: Add Country, State and District Listing APIs

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from app.api.user_routes import router as user_router
+from app.api.public_routes import router as public_router
 
 app_router = APIRouter()
 
 app_router.include_router(user_router)
+app_router.include_router(public_router)
